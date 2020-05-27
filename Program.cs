8c88970cdc312c4fcba0026b40647b0b89a8409f
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-27 18:29:50 -0300

    First Verson

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_ou_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int resultado;
+            
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            resultado = RetornoImparPar(numero);
+
+            if (resultado == 1)
+                Console.WriteLine("O número informado é impar");
+            else
+                Console.WriteLine("O número informado é par");
+
+            Console.ReadLine();
+        }
+
+        public static int RetornoImparPar(int numero)
+        {
+            int resultado = numero % 2;
+            return resultado;
+        }
+    }
+}
